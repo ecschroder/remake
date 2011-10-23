b63a66204958f5f3e14e03d22ce6d28a21668b92
Emma Schroder <ecschroder@gmail.com>
2011-10-22 17:11:34 -0700

    just practicing

--- urls.py
+++ urls.py
@@ -1,4 +1,8 @@
+from django.conf import settings
+
 from django.conf.urls.defaults import patterns, include, url
+
+from trashure.models import KnickKnack
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -11,18 +15,18 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+   
     # Uncomment the next line to enable the admin:
 	url(r'^admin/', include(admin.site.urls)),
 	
 	url(
-		regex = "^trashure/$",
+		regex = r"^trashure/$",
 		view = trashure.views.index,
 		name = "index",
 	),
 
 	url (
-		regex = "^trashure/(?P<knickknack_id>\d+)/$",
+		regex = r"^trashure/(?P<knickknack_id>\d+)/$",
 		view = trashure.views.detail,
 		name = "detail",
 	),
